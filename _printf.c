0189d11796c7cd135eea30e10dc4afe7d1389aaf
Sandra Mukidza <sandramukidza.com>
2023-03-25 11:20:48 -0700

    betty fix

--- _printf.c
+++ _printf.c
@@ -8,75 +8,78 @@
 
 int _putchar(char c)
 {
-       	return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
 
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list arg;
-    
-    va_start(arg, format);
-    
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch(*format)
-            {
-                case 'c':
-                   {
-                       char c = (char) va_arg(arg, int);
-                       _putchar(c);
-                       count++;
-                       break;
-                   }
-                case 's':
-                   {
-                       char *s = va_arg(arg, char *);
-                       while (*s++)
-                       {
-                             _putchar(*(s-1));
-                             count++;
-                       }
-                       break;
-                   }
-                case '%':
-                    {
-                        _putchar('%');
-                        count++;
-                        break;
-                    }
-                case 'd':
-                case 'i':
-                {
-                    int intv = va_arg(arg, int);
-                    if (intv < 0) {
-                          _putchar('-');
-                          count++;
-                          intv = -intv;
-                        }
-                     if (intv / 10) {
-                        count += _printf("%d", intv / 10);
-                       }
-                       _putchar(intv % 10 + '0');
-                               count++;
-                               break;
-                }
-                             
-                default:
-                    break;
-                    
-            }
-        }else
-        {
-            _putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(arg);
-    return count;
+	int count = 0;
+	va_list arg;
+
+	va_start(arg, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+		switch (*format)
+		{
+			case 'c':
+				{
+					char c = (char) va_arg(arg, int);
+
+					_putchar(c);
+					count++;
+					break;
+				}
+			case 's':
+				{
+					char *s = va_arg(arg, char *);
+
+					while (*s++)
+					{
+						_putchar(*(s - 1));
+						count++;
+					}
+					break;
+				}
+			case '%':
+				{
+					_putchar('%');
+					count++;
+					break;
+				}
+			case 'd':
+			case 'i':
+				{
+					int intv = va_arg(arg, int);
+
+					if (intv < 0)
+					{
+						_putchar('-');
+						count++;
+						intv = -intv;
+					}
+					if (intv / 10)
+					{
+						count += _printf("%d", intv / 10);
+					}
+					_putchar(intv % 10 + '0');
+					count++;
+					break;
+				}
+			default:
+				break;
+		}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(arg);
+	return (count);
 }
