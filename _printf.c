6ea3c17841993915f1fdce30607d7c4ba8702215
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-25 20:48:30 +0100

    betty fixes: code refactoring

--- _printf.c
+++ _printf.c
@@ -1,23 +1,31 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - print a character
- *@: charcter to print
+ *@c: charcter to print
+ *
+ * Return: count
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+/**
+ * _printf - produce output accoring to format
+ * @format: input parmeter
+ *
+ * Return: count of input
+ */
 int _printf(const char *format, ...)
 {
 	int count = 0;
+	char c, *s;
 	va_list arg;
 
 	va_start(arg, format);
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -26,51 +34,22 @@ int _printf(const char *format, ...)
 		switch (*format)
 		{
 			case 'c':
-				{
-					char c = (char) va_arg(arg, int);
-
+					c = (char) va_arg(arg, int);
 					_putchar(c);
 					count++;
 					break;
-				}
 			case 's':
-				{
-					char *s = va_arg(arg, char *);
-
-					while (*s++)
-					{
-						_putchar(*(s - 1));
-						count++;
-					}
+					s = va_arg(arg, char *);
+					count += _puts(s, count);
 					break;
-				}
 			case '%':
-				{
 					_putchar('%');
 					count++;
 					break;
-				}
 			case 'd':
 			case 'i':
-				{
-					int intv = va_arg(arg, int);
-
-					if (intv < 0)
-					{
-						_putchar('-');
-						count++;
-						intv = -intv;
-					}
-					if (intv / 10)
-					{
-						count += _printf("%d", intv / 10);
-					}
-					_putchar(intv % 10 + '0');
-					count++;
+					count += print_int(va_arg(arg, int), count);
 					break;
-				}
-			default:
-				break;
 		}
 		}
 		else
@@ -81,5 +60,45 @@ int _printf(const char *format, ...)
 		format++;
 	}
 	va_end(arg);
+	return (count);
+}
+/**
+ * print_int - print integer to stdout and count
+ * @intv: integer to print
+ * @count: integer digits
+ *
+ * Return: integer count
+ */
+
+int print_int(int intv, int count)
+{
+	if (intv < 0)
+	{
+		_putchar('-');
+		count++;
+		intv = -intv;
+	}
+	if (intv / 10)
+	{
+		count += _printf("%d", intv / 10);
+	}
+	_putchar(intv % 10 + '0');
+	count++;
+	return (count);
+}
+/**
+ * _puts - prints characters of a string
+ * @s: string to print
+ * @count: count of string
+ *
+ * Return: count of string
+ */
+int _puts(char *s, int count)
+{
+	while (*s++)
+	{
+		_putchar(*(s - 1));
+		count++;
+	}
 	return (count);
 }
--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdio.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_int(int intv, int count);
+int _puts(char *s, int count);
 
 #endif
