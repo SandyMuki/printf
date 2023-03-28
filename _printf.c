15a74695a58a291e4d2b1569287e45736e08e2a3
Sandra Mukidza <sandramukidza.com>
2023-03-28 02:40:48 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -69,22 +69,26 @@ int _printf(const char *format, ...)
  * Return: integer count
  */
 
-int print_int(int n) {
+int print_int(int n)
+{
 	int counter = 0;
 	int rev = 0;
 
-	if (n < 0) {
+	if (n < 0)
+	{
 		_putchar('-');
 		n = -n;
-		counter +=1;
+		counter += 1;
 	}
 
-	while (n > 0) {
+	while (n > 0)
+	{
 		rev = rev * 10 + n % 10;
 		n /= 10;
 	}
 
-	while (rev > 0) {
+	while (rev > 0)
+	{
 		_putchar(rev % 10 + '0');
 		rev /= 10;
 		counter += 1;
