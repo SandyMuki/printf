1f228a82ae7be462dc4a6bee97261cda91747ae5
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-28 10:24:48 +0100

    fix: modfity _printf to count characters properly

--- _printf.c
+++ _printf.c
@@ -31,26 +31,26 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-		switch (*format)
-		{
-			case 'c':
+			switch (*format)
+			{
+				case 'c':
 					c = (char) va_arg(arg, int);
 					_putchar(c);
 					count++;
 					break;
-			case 's':
+				case 's':
 					s = va_arg(arg, char *);
-					count += _puts(s, count);
+					count += _puts(s);
 					break;
-			case '%':
+				case '%':
 					_putchar('%');
 					count++;
 					break;
-			case 'd':
-			case 'i':
-					count += print_int(va_arg(arg, int), count);
+				case 'd':
+				case 'i':
+					count += print_int(va_arg(arg, int));
 					break;
-		}
+			}
 		}
 		else
 		{
@@ -64,41 +64,47 @@ int _printf(const char *format, ...)
 }
 /**
  * print_int - print integer to stdout and count
- * @intv: integer to print
- * @count: integer digits
+ * @n: integer to print
  *
  * Return: integer count
  */
 
-int print_int(int intv, int count)
-{
-	if (intv < 0)
-	{
+int print_int(int n) {
+	int counter = 0;
+	int rev = 0;
+
+	if (n < 0) {
 		_putchar('-');
-		count++;
-		intv = -intv;
+		n = -n;
+		counter +=1;
 	}
-	if (intv / 10)
-	{
-		count += _printf("%d", intv / 10);
+
+	while (n > 0) {
+		rev = rev * 10 + n % 10;
+		n /= 10;
 	}
-	_putchar(intv % 10 + '0');
-	count++;
-	return (count);
+
+	while (rev > 0) {
+		_putchar(rev % 10 + '0');
+		rev /= 10;
+		counter += 1;
+	}
+	return (counter);
 }
 /**
  * _puts - prints characters of a string
  * @s: string to print
- * @count: count of string
  *
  * Return: count of string
  */
-int _puts(char *s, int count)
+int _puts(char *s)
 {
+	int counter = 0;
+
 	while (*s++)
 	{
 		_putchar(*(s - 1));
-		count++;
+		counter++;
 	}
-	return (count);
+	return (counter);
 }
